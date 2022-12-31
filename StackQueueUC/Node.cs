679bc963f3f7c5f772bee67bd1ebb9c4549dcd24
patelaftab <10patelaftab@gmail.com>
2022-12-31 20:17:07 +0530

    [Aftab] Creating Stack

---
+++ StackQueueUC/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueueUC
+{
+    public class Node
+    {
+
+        public Node next;
+        public int data;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ StackQueueUC/Program.cs
@@ -0,0 +1,29 @@
+﻿using StackQueueUC;
+
+namespace StackQueue
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Stack And Queue Program ");
+            Console.WriteLine("===============================");
+            Console.WriteLine("Choose a option from below\n" +
+                "\nChoose 1): To Creating a Stack  \n");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    LinkedListStack stackObject = new LinkedListStack();
+                    stackObject.Push(70);
+                    stackObject.Push(30);
+                    stackObject.Push(56);
+                    stackObject.Display();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+}
